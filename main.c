5ec2b3c3326b99a7f46b44fab8eafb577a56fc89
sss <soe@ss.ss>
2016-04-16 19:37:26 -0400

    now work with multibyte unicode

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 int main(int argc, char **argv){
   if(argc<2){
-    fprintf(stderr, "File name not specified");
+    perror("File name not specified");
     return (-1);
   }
 
+  setlocale(LC_ALL, "");
+
   const char *tmp_str=".tmp";
   const ssize_t tmp_str_len=strlen(tmp_str);
-  const size_t str_len=79;
+  const size_t str_len=80;
+  wchar_t *str=malloc(sizeof(wchar_t)*str_len);
     
   FILE *fp;
   FILE *fph;
@@ -20,33 +24,73 @@ int main(int argc, char **argv){
   char *tmp_file_name;
   size_t tmp_file_name_len;
 
+  size_t mbslen;
+  wchar_t *wcs;
   size_t j;
-  size_t line_len;
+  size_t k;
   for (i = 1; i < argc; i++) {
 
     fp=fopen(argv[i], "r");
     if(fp==NULL){
       fprintf(stderr, "Can't open file: %s\n", argv[i]);
-      return (-1);
+      exit(EXIT_FAILURE);
     }    
 
     tmp_file_name_len=strlen(argv[i]) + tmp_str_len;
     tmp_file_name=malloc(sizeof(char) * tmp_file_name_len);
+    if (tmp_file_name == NULL) {
+      perror("malloc");
+      exit(EXIT_FAILURE);
+    }
+
     strcpy(tmp_file_name, argv[i]);
     strcat(tmp_file_name, tmp_str);
     
     fph=fopen(tmp_file_name, "w+");
     if(fp==NULL){
       fprintf(stderr, "Can't open/create file: %s\n", tmp_file_name);
-      return (-1);
+      exit(EXIT_FAILURE);
     }
-    fflush(fph);
-    while((read=getline(&line,&len,fp))!=-1){
-      line_len=strlen(line);
-      for (j=0;j<line_len; j+=str_len) {
-	fwrite(line+j,str_len,sizeof(char),fph);
-	fwrite("\n",1,sizeof(char),fph);
+
+
+    
+    read=getline(&line,&len,fp);
+    while(read!=-1){
+
+      mbslen = mbstowcs(NULL, line, 0);
+      if (mbslen == (size_t) -1) {
+	perror("mbstowcs");
+	exit(EXIT_FAILURE);
       }
+
+      if(mbslen > str_len){
+	wcs=calloc(mbslen, sizeof(wchar_t));
+	if (wcs == NULL) {
+	  perror("calloc");
+	  exit(EXIT_FAILURE);
+	}
+	if (mbstowcs(wcs, line, mbslen) == (size_t) -1) {
+	  perror("mbstowcs");
+	  exit(EXIT_FAILURE);
+	}
+
+	for (j=0; j<mbslen; j+=str_len) {
+	  k=mbslen-str_len*j;
+	  if (k>str_len) {
+	    k=str_len;
+	  }
+	  wcsncpy(str, wcs+j,k); 
+	  fprintf(fph,"%ls\n",str);
+	}
+
+
+      }
+      else{
+
+	fprintf(fph,"%s\n",line);
+      }
+	   
+      read=getline(&line,&len,fp);
     }
     fclose(fp);
     fclose(fph);
@@ -55,6 +99,7 @@ int main(int argc, char **argv){
   //if (line) {
   //free(line);
   //}
+  //free(str);
   printf("\n");
-  return (1);
+  exit(EXIT_SUCCESS);
 }
